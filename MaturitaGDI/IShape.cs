2aa34686e214c6eaa1ba7f5b56aa64295ffab111
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-16 14:47:07 +0200

    interface

---
+++ MaturitaGDI/IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaturitaGDI
+{
+    public interface IShape
+    {
+        Point Point { get; set; }
+        Pen Pen { get; set; }
+        SolidBrush SolidBrush { get; set; }
+        void Draw(params object[] objects);
+    }
+}
